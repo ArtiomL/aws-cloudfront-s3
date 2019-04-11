bd87e29ea8a219f0616e821b27db8ab1423c866f
Artiom Lichtenstein <3167332+ArtiomL@users.noreply.github.com>
2019-04-11 20:20:16 +0300

    Create index.js

---
+++ index.js
@@ -0,0 +1,19 @@
+'use strict';
+
+exports.handler = async (event) => {
+	return new Promise((resolve) => {
+
+		const response = event.Records[0].cf.response;
+		const headers = response.headers;
+
+		headers['Strict-Transport-Security'] = [{ key: 'Strict-Transport-Security', value: 'max-age=63072000; includeSubDomains; preload' }];
+		headers['X-XSS-Protection'] = [{ key: 'X-XSS-Protection', value: '1; mode=block' }];
+		headers['X-Content-Type-Options'] = [{ key: 'X-Content-Type-Options', value: 'nosniff' }];
+		headers['X-Frame-Options'] = [{ key: 'X-Frame-Options', value: 'DENY' }];
+		headers['Referrer-Policy'] = [{	key: 'Referrer-Policy',	value: 'no-referrer' }];
+		headers['Feature-Policy'] = [{ key: 'Feature-Policy', value: 'geolocation none; midi none; notifications none; push none; sync-xhr none; microphone none; camera none; magnetometer none; gyroscope none; speaker self; vibrate none; fullscreen self; payment none;' }];
+
+		resolve(response);
+
+	});
+};
