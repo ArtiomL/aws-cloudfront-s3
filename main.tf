e3b26783f994897286d46b4b1311dbc160d82bdb
Artiom Lichtenstein <3167332+ArtiomL@users.noreply.github.com>
2019-05-03 19:35:21 +0300

    Infrastructure

--- main.tf
+++ main.tf
@@ -239,11 +239,23 @@ data "archive_file" "main" {
   output_path = "source.zip"
 }
 
-# DNS alias record
+# DNS alias records
 resource "aws_route53_record" "main" {
   zone_id = "${var.zone_id}"
   name    = "${var.domain_name}"
   type    = "A"
+
+  alias {
+    name                   = "${aws_cloudfront_distribution.main.domain_name}"
+    zone_id                = "${aws_cloudfront_distribution.main.hosted_zone_id}"
+    evaluate_target_health = false
+  }
+}
+
+resource "aws_route53_record" "ipv6" {
+  zone_id = "${var.zone_id}"
+  name    = "${var.domain_name}"
+  type    = "AAAA"
 
   alias {
     name                   = "${aws_cloudfront_distribution.main.domain_name}"
