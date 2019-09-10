b0be6fb79f8b66b3be2f56842f58fbb840fb3aa5
Artiom Lichtenstein <3167332+ArtiomL@users.noreply.github.com>
2019-09-10 03:51:49 +0300

    Infrastructure

--- example/main.tf
+++ example/main.tf
@@ -5,6 +5,7 @@ module "aws_cloudfront_s3" {
   source          = "github.com/ArtiomL/aws-cloudfront-s3"
   aws_region      = "eu-west-1"
   domain_name     = "example.com"
+  web_acl_id      = "a123fae4-b567-8e90-1234-5ab67ac8ca90"
   zone_id         = "Z3P5QSUBK4POTI"
   tag_name        = "Example"
   tag_environment = "Dev"
@@ -20,6 +21,6 @@ resource "null_resource" "main" {
 # Slack notification
 module "slack" {
   source       = "github.com/ArtiomL/aws-terraform/modules/common/slack"
-  message_text = "Deployed: ${module.aws_cloudfront_s3.bucket_id} as ${module.aws_cloudfront_s3.dist_domain}"
+  message_text = "Deployed: ${module.aws_cloudfront_s3.alias_fqdn} as ${module.aws_cloudfront_s3.dist_domain}"
   webhook_url  = "T00000000/B00000000/XXXXXXXXXXXXXXXXXXXXXXXX"
 }
