c831cf64acbcad4e5ea29334a6d70a8b3a1bdd24
Artiom Lichtenstein <3167332+ArtiomL@users.noreply.github.com>
2019-10-12 18:03:46 +0300

    Output Variables

--- outputs.tf
+++ outputs.tf
@@ -2,40 +2,41 @@
 
 output "bucket_id" {
   description = "S3 bucket name"
-  value       = "${aws_s3_bucket.main.id}"
+  value       = aws_s3_bucket.main.id
 }
 
 output "bucket_regional" {
   description = "S3 bucket region-specific domain name, also used as CloudFront Origin ID"
-  value       = "${aws_s3_bucket.main.bucket_regional_domain_name}"
+  value       = aws_s3_bucket.main.bucket_regional_domain_name
 }
 
 output "cert_arn" {
   description = "ACM certificate ARN"
-  value       = "${aws_acm_certificate.main.arn}"
+  value       = aws_acm_certificate.main.arn
 }
 
 output "dist_id" {
   description = "CloudFront distribution ID"
-  value       = "${aws_cloudfront_distribution.main.id}"
+  value       = aws_cloudfront_distribution.main.id
 }
 
 output "dist_arn" {
   description = "CloudFront distribution ARN"
-  value       = "${aws_cloudfront_distribution.main.arn}"
+  value       = aws_cloudfront_distribution.main.arn
 }
 
 output "dist_domain" {
   description = "CloudFront distribution domain name"
-  value       = "${aws_cloudfront_distribution.main.domain_name}"
+  value       = aws_cloudfront_distribution.main.domain_name
 }
 
 output "dist_zone_id" {
   description = "CloudFront zone ID that can be used to point Route 53 alias records to"
-  value       = "${aws_cloudfront_distribution.main.hosted_zone_id}"
+  value       = aws_cloudfront_distribution.main.hosted_zone_id
 }
 
 output "alias_fqdn" {
   description = "DNS alias record FQDN"
-  value       = "${aws_route53_record.main.fqdn}"
+  value       = aws_route53_record.main.fqdn
 }
+
