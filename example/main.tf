76bea3c1801a86826f8d5a6e5eaadaca569f3cae
Artiom Lichtenstein <3167332+ArtiomL@users.noreply.github.com>
2020-08-06 11:14:23 +0300

    Infrastructure

--- example/main.tf
+++ example/main.tf
@@ -1,14 +1,24 @@
 # Infrastructure
 
+provider "aws" {
+  region = "eu-west-1"
+}
+
 # S3, IAM, ACM, CloudFront
 module "aws_cloudfront_s3" {
   source          = "github.com/ArtiomL/aws-cloudfront-s3"
   aws_region      = "eu-west-1"
   domain_name     = "example.com"
-  web_acl_id      = "a123fae4-b567-8e90-1234-5ab67ac8ca90"
+  web_acl_id      = aws_wafv2_web_acl.main.arn
   zone_id         = "Z3P5QSUBK4POTI"
   tag_name        = "Example"
   tag_environment = "Dev"
+}
+
+# AWS Shield Advanced
+resource "aws_shield_protection" "main" {
+  name         = "shieldExampleDev"
+  resource_arn = module.aws_cloudfront_s3.dist_arn
 }
 
 # Upload local files to S3
@@ -24,4 +34,3 @@ module "slack" {
   message_text = "Deployed: ${module.aws_cloudfront_s3.alias_fqdn} as ${module.aws_cloudfront_s3.dist_domain}"
   webhook_url  = "T00000000/B00000000/XXXXXXXXXXXXXXXXXXXXXXXX"
 }
-
